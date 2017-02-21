e8acb15ec8edc01ec1e07d28ecdac4d0e791e22f
djiordhan <jordandiamante@icloud.com>
2017-02-21 22:48:06 +0800

    Add image services files

---
+++ utils/ImageResizer.js
@@ -0,0 +1,9 @@
+/**
+ * Created by jordy on 21/01/2017.
+ */
+
+module.exports = {
+    resizeByDimension : function(dimension, image, isBigger, adjust) {
+
+    }
+};
---
+++ utils/ImageUtils.js
@@ -0,0 +1,14 @@
+/**
+ * Created by jordy on 21/01/2017.
+ */
+
+// @params: 24 sets of integers grabbed from rgbs of 8 images,
+// This function is part of decoding process that analyze the set of integers
+// if an integer is even we decode it as 1 and if odd its 0
+exports.getIntFromBinary = function(rgbs) {
+    var n = "";
+    for (var i = 0; i < rgbs.length; i++) {
+
+    }
+
+};
